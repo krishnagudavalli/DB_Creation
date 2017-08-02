22190d109f92741818d00967bdf20be753adc1dc
Mounika Munnaluri <mmunnaluri@quadyster.com>
2017-08-01 19:20:38 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# DB_Creation
-This is a Database Creation Project. Which helps to Create postgress DB using Cloud Formation Template.
+# postgres Database Creation
+
+
+ This is a postgres Database Creation Project. Which helps to Create postgress DB using Cloud Formation Template.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='postgres_db_creation',
+    version='0.1.0',
+    description='Python module for importing cloudFormation template, validating and creating a cloudFormation stack.',
+    long_description=readme,
+    author='Quadyster_dev_team',
+    author_email='mmunnalluri'@quadyster.com',
+    url='https://github.com/mmunnaluri/DB_Creation.git',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
